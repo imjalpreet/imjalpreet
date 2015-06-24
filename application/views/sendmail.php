2d257eb61cffdc6dde31197208521444c344cd5f
Jalpreet Singh Nanda (imjalpreet) <jalpreetnanda@gmail.com>
2015-06-25 00:57:33 +0530

    [ContactMe] Visitor is replied now through an email

--- application/views/sendmail.php
+++ application/views/sendmail.php
@@ -19,21 +19,45 @@
     //Set TCP port to connect to
     $mail->Port = 587;
 
+    $Mail = new PHPMailer(true);
+
+    //Enable SMTP debugging.
+    $Mail->SMTPDebug = 0;
+    //Set PHPMailer to use SMTP.
+    $Mail->isSMTP();
+    //Set SMTP host name
+    $Mail->Host = "smtp.gmail.com";
+    //Set this to true if SMTP host requires authentication to send email
+    $Mail->SMTPAuth = true;
+    //Provide username and password
+    $Mail->Username = "contact.imjalpreet@gmail.com";
+    $Mail->Password = "contactjalpreet";
+    //If SMTP requires TLS encryption then set it
+    $Mail->SMTPSecure = "tls";
+    //Set TCP port to connect to
+    $Mail->Port = 587;
+
     $mail->addReplyTo($_POST['email'], $_POST['name']);
     $mail->From = "contact.imjalpreet@gmail.com";
     $mail->FromName = 'Me';
+    $Mail->From = "contact.imjalpreet@gmail.com";
+    $Mail->FromName = 'Jalpreet Singh Nanda';
 
     $mail->addAddress("contact.imjalpreet@gmail.com", "Jalpreet Singh Nanda");
+    $Mail->addAddress($_POST['email'], $_POST['name']);
 
     $mail->isHTML(true);
 
     $mail->Subject = $_POST['subject'];
     $mail->Body = $_POST['message'] ;
     //$mail->AltBody = $_POST['message'];
-
+    $Mail->isHTML(true);
+    $Mail->Subject = "Re: ".$_POST['subject'];
+    $Mail->Body = "<i><b>Hey ".$_POST['name'].", Thank you for contacting me! I will try to respond to you as soon as possible.</b></i>" ;
     if(!$mail->send()) {
         echo "Mailer Error: " . $mail->ErrorInfo; // ... or this one to tell it that it wasn't sent
     }else{
        echo 'sent';
     }
+    $Mail->send();
 ?>
